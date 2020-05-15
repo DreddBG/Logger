7348426ea6815c09774b3b7e82be5e8ff88dc9a3
Stoyan Lazarov <ninjakiviWTF@gmail.com>
2020-05-15 03:25:55 +0300

    File logging ability added.

--- Models/Appenders/FileAppender.cs
+++ Models/Appenders/FileAppender.cs
@@ -1,20 +1,30 @@
 ﻿using System;
+using System.IO;
+using System.Text;
 using LoggerLib.Contracts;
 
 namespace LoggerLib.Models.Appenders
 {
     public class FileAppender : IAppender
     {
+        private ILayout layout;
+        private string _filePath;
+
         public ReportLevel ReportLevel { get; set; } = 0;
 
-        public FileAppender()
+        public FileAppender(ILayout layout, string filePath)
         {
-
+            this.layout = layout;
+            this._filePath = filePath;
         }
 
         public void WriteLine(ILog input)
         {
-            throw new NotImplementedException();
+            if (input.Level >= this.ReportLevel)
+            {
+                var output = this.layout.Format(input);
+                File.AppendAllTextAsync(this._filePath, output + "\n");
+            }
         }
     }
 }
